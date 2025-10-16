15b0f7be4d06b97dae022868c869d1be90e860bb
chandanray917 <chandan128902@gmail.com>
2025-10-16 12:56:52 +0530

    learning typecasting

--- src/B_javaVeriable.java
+++ src/B_javaVeriable.java
@@ -7,14 +7,26 @@ public class B_javaVeriable {
         System.out.println(marks);
         // premitive datatypes:
 //             Datatype       Size        Default
-        //        int       ->4/32 byte     0
-        //        float     ->4/32 bite     0.0f
-        //        boolean   ->1    byte     false
-        //        char      ->2/16 byte     '\u0000'
-        //        double    ->8/64 byte     0.0d
-        //        long      ->8/64 byte     0l
-        //        short     ->2/16 byte     0
-        //        byte      ->1/8  bite     0
+        //        int       ->4 byte     0
+        //        float     ->4 bite     0.0f
+        //        boolean   ->1 byte     false
+        //        char      ->2 byte     '\u0000'
+        //        double    ->8 byte     0.0d
+        //        long      ->8 byte     0l
+        //        short     ->2 byte     0
+        //        byte      ->1 bite     0
+
+        boolean isPassed = true;
+        int count = 12;
+        byte ismarks = 34;
+        float pi = 3.12f;
+        byte newAGe = (byte)count; // typecasting
+
+        System.out.println(count);
+
+        /*multiline comment*/
+        /* ctrl + shift + /*/
+        // ctrl + /
 
 
 
