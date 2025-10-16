b99a1f8034b4f96815e8c9f7eba5cedd5475599a
chandanray917 <chandan128902@gmail.com>
2025-10-16 12:27:37 +0530

    learned java variables and it's data types

---
+++ src/A_firstLecture.java
@@ -0,0 +1,57 @@
+public class A_firstLecture { //this is a block
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        System.out.println(4+3);
+        System.out.println(3 * 3);
+
+    }
+}
+
+// --------------------------java keywords
+// abstract
+// continue
+// for
+// new
+// switch
+// assert
+// default
+// goto
+// package
+// synchronized
+// boolean
+// do
+// if
+// private
+// this
+// break
+// double
+// implements
+// protected
+// throw
+// case
+// enum
+// instanceof
+// return
+// transient
+// catch
+// extends
+// int
+// short
+// try
+// char
+// final
+// interface
+// static
+// void
+// class
+// finally
+// long
+// strictfp
+// volatile
+// const
+// float
+// native
+// super
+// while
+
+
---
+++ src/B_javaVeriable.java
@@ -0,0 +1,23 @@
+public class B_javaVeriable {
+    public static  void main(String[] args){
+        int age = 21;
+        int marks = 88;
+        int $marks = 88;
+
+        System.out.println(marks);
+        // premitive datatypes:
+//             Datatype       Size        Default
+        //        int       ->4/32 byte     0
+        //        float     ->4/32 bite     0.0f
+        //        boolean   ->1    byte     false
+        //        char      ->2/16 byte     '\u0000'
+        //        double    ->8/64 byte     0.0d
+        //        long      ->8/64 byte     0l
+        //        short     ->2/16 byte     0
+        //        byte      ->1/8  bite     0
+
+
+
+    }
+
+}
